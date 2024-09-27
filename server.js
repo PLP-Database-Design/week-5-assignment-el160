fa8188a28cb3c547f74f85f99b122407df45b180
el160 <ljhongeri04@gmail.com>
2024-09-28 00:23:29 +0300

    this is week 5 assignment

---
+++ server.js
@@ -0,0 +1,99 @@
+//1. setting up dependancies
+const express =require('express');
+const app = express();
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
+
+
+// // 4.CONFIGURE ENVIRONMENTAL VARIABLES
+
+dotenv.config();
+
+//  //5. CREATING CONNECTION OBJECT(important part)
+ const db = mysql.createConnection(
+   {
+      host:process.env.DB_HOST,
+       user:process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD,
+     database: process.env.DB_NAME
+  }
+);
+
+
+// // 6. Check if const db Connection works
+
+ db.connect((err) =>{
+  //  // doesnt connect
+   if(err){ return console.log("Error connecting to mysql database")};
+// // does connect
+   console.log("connected to mysql successfully as id:",db.threadId)
+ });
+
+
+// 3.. to see if it works on browser- basic endpoint
+ //app.get('' , (req, res)  =>{
+ //  res.send("hello world, how are you feeling thank you")
+ //});
+
+// Question 1  Retrieve all patients
+
+app.get('', (req, res)=>{
+  const getPatients = 'SELECT * FROM Patients'
+  db.query (getPatients,(err,data)=>{
+    //if there is an error
+   if(err){
+   return res.status(400).send('Failed to Retrieve data',err)
+   }
+ res.status(200).send(data)
+  });
+});
+
+
+// Question 2 Retrieve all providers
+app.get('',(req,res)=>{
+  const getProviders = ' SELECT first_name,last_name,provider_specialty FROM Providers'
+  db.query( getProviders,(err,data)=>{
+    if(err){
+      return res.status(400).send('Failed to retrieve results',err)
+    }
+    res.status(200).send(data)
+  });
+});
+
+
+//Question  3 Filter patients by First Name
+app.get('', (req,res)=>{
+const getPatients = 'SELECT first_name FROM Patients'
+db.query(getPatients, (err,data)=>{
+  if(err){
+    return res.status(400).send('Failed to get data',err)
+  }
+  res.status(200).send(data)
+});
+});
+
+
+// Question 4 Retrieve all providers by their specialty
+
+app.get('', (req, res) => {
+  const getProviders = 'SELECT provider_specialty, first_name FROM providers ORDER BY provider_specialty'
+    
+
+  db.query(getProviders, (err, data) => {
+    if (err) {
+      return res.status(400).send('Failed to retrieve providers' , err);
+    }
+    res.status(200).send(data); 
+  });
+});
+
+  
+  
+
+
+// 2.listen to the server
+//const PORT = 3001
+app.listen(3000, () => {
+  console.log(`server is running on port 3000...`)
+});
+ 
