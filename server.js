6b3828ee7d819eec7b34ffebbd3f912ec82a5b7c
elnyamwea <ljhongeri04@gmail.com>
2024-10-03 12:47:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,26 +31,28 @@ dotenv.config();
 
 
 // 3.. to see if it works on browser- basic endpoint
- //app.get('' , (req, res)  =>{
+ //app.get('/data' , (req, res)  =>{
  //  res.send("hello world, how are you feeling thank you")
  //});
 
 // Question 1  Retrieve all patients
 
-app.get('', (req, res)=>{
-  const getPatients = 'SELECT * FROM Patients'
-  db.query (getPatients,(err,data)=>{
-    //if there is an error
-   if(err){
-   return res.status(400).send('Failed to Retrieve data',err)
-   }
- res.status(200).send(data)
-  });
-});
+
+app.get('/get-patients', (req, res) => {
+  const getPatients = "SELECT * FROM patients"
+  db.query(getPatients, (err, results) => {
+      // have an error
+      if(err) {
+          return res.status(500).send("Failed to fetch the patients")
+      }
+      // get back the data/results
+      res.status(200).send(results)
+  })
+})
 
 
 // Question 2 Retrieve all providers
-app.get('',(req,res)=>{
+app.get('/get-providers',(req,res)=>{
   const getProviders = ' SELECT first_name,last_name,provider_specialty FROM Providers'
   db.query( getProviders,(err,data)=>{
     if(err){
@@ -62,7 +64,7 @@ app.get('',(req,res)=>{
 
 
 //Question  3 Filter patients by First Name
-app.get('', (req,res)=>{
+app.get('/get-patients', (req,res)=>{
 const getPatients = 'SELECT first_name FROM Patients'
 db.query(getPatients, (err,data)=>{
   if(err){
@@ -75,8 +77,8 @@ db.query(getPatients, (err,data)=>{
 
 // Question 4 Retrieve all providers by their specialty
 
-app.get('', (req, res) => {
-  const getProviders = 'SELECT provider_specialty, first_name FROM providers ORDER BY provider_specialty'
+app.get('/get-providers', (req, res) => {
+  const getProviders = 'SELECT provider_specialty,first_name  FROM providers ORDER BY provider_specialty '
     
 
   db.query(getProviders, (err, data) => {
@@ -92,8 +94,9 @@ app.get('', (req, res) => {
 
 
 // 2.listen to the server
-//const PORT = 3001
-app.listen(3000, () => {
-  console.log(`server is running on port 3000...`)
+//const PORT = 3500
+app.listen(3500, () => {
+  console.log(`server is running on port 3500...`)
 });
+ 
  
